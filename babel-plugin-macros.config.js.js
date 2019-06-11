b0f82f9b2b486de46bb58e6c00c70d71bc2647cb
celinechole <github@celinechole.com>
2019-06-10 21:02:21 -0400

    add tailwind and emotion

---
+++ babel-plugin-macros.config.js.js
@@ -0,0 +1,5 @@
+module.exports = {
+  tailwind: {
+    config: './src/tailwind.config.js', // if you added a config file
+  },
+};
--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import tw from 'tailwind.macro';
+import styled from '@emotion/styled'
+
+const Header = styled.header`
+${tw`bg-red-300 min-h-screen flex flex-col items-center justify-center text-xl text-white`};
+`;
 
 function App() {
   return (
-    <div className="App">
-      HEY
+    <div>
+      <Header>
+        PLANT TRACKER
+      </Header>
+      <main>
+        This is the beginning
+      </main>
     </div>
   );
 }
