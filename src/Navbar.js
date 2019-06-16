1b4639d99d4055fc3ec921ac6ed6ef2f2e1ae2f7
celinechole <github@celinechole.com>
2019-06-16 13:51:08 -0400

    updated class name to css tailwind

--- src/Navbar.js
+++ src/Navbar.js
@@ -12,17 +12,19 @@ const NavLink = styled.a`
   ${tw`block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4`};
 `;
 
+
+
 function Navbar() {
   return (
     <React.Fragment>
       <Nav>
-        <div className="flex items-center flex-shrink-0 text-white mr-7">
-          <span className="font-semibold text-xl tracking-tight">
-            Tailwind CSS
+        <div css={tw`flex items-center flex-shrink-0 text-white mr-7`}>
+          <span css={tw`font-semibold text-xl tracking-tight`}>
+            Plant Tracker
           </span>
         </div>
-        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <div className="text-sm lg:flex-grow">
+        <div css={tw`w-full block flex-grow lg:flex lg:items-center lg:w-auto`}>
+          <div css={tw`text-sm lg:flex-grow`}>
             <NavLink href="#responsive-header">Docs</NavLink>
             <NavLink href="#responsive-header">Docs1</NavLink>
             <NavLink href="#responsive-header">Docs2</NavLink>
@@ -30,7 +32,7 @@ function Navbar() {
           <div>
             <a
               href="#"
-              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+              css={tw`inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0`}
             >
               Download
             </a>
