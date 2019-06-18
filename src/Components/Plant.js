621f9acec5c9daad828a79c2b073ff4a3c52b9c0
celinechole <github@celinechole.com>
2019-06-18 15:24:07 -0400

    create first structure for plant card

--- src/Components/Plant.js
+++ src/Components/Plant.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { getPlant } from "./PlantStore";
+import tw from "tailwind.macro";
+import styled from "@emotion/styled";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
 
 const Plant = props => {
   const [plant, setPlant] = useState(null);
@@ -11,13 +15,16 @@ const Plant = props => {
   return (
     <React.Fragment>
       {plant ? (
-        <div>
+        <div css={tw`flex mb-4`}>
+        <div css={tw`w-1/2`}><img src={plant.img} /></div>
+        <div css={tw`w-1/2`}>
           <p>{plant.name}</p>
           <p>{plant.id}</p>
           <p>{plant.purchased}</p>
           <p>{plant.light}</p>
           <p>{plant.water}</p>
         </div>
+      </div>
       ) : (
         <div>You are out of green</div>
       )}
