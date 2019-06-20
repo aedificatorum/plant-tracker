c52cfe436364c67b119896d7c3cd8b94f9272023
celinechole <github@celinechole.com>
2019-06-19 21:22:04 -0400

    move link to only be active on plant nickname

--- src/Components/PlantList.js
+++ src/Components/PlantList.js
@@ -10,27 +10,43 @@ const PlantList = () => {
   const [watered, setWatered] = useState(false);
 
   const dailyWater = getPlants().filter(plant => {
-    return plant.water === 'daily'
-  })
-
-
+    return plant.water === "daily";
+  });
 
   const allPlants = getPlants().map(plant => {
     return (
       <div key={plant.name}>
-        <Link to={`plant/${plant.id}`}>
-          <div css={tw`md:flex p-12`}>
-            <div css={tw`md:flex-shrink-0`}>
-              <img css={tw`rounded-lg md:w-56`} src="https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_snake-plant-slaurentii_terracotta_4_1024x1024.jpg?v=1537308389" alt="Snake plant" />
-            </div>
-            <div css={tw`mt-4 md:mt-0 md:ml-6 align-middle`}>
-              <div css={tw`uppercase tracking-wide text-sm text-indigo-600 font-bold`}>{plant.nickname}</div>
-              <div href="#" css={tw`block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline`}>{plant.water}</div>
-              <p css={tw`mt-2 text-gray-600`}>{plant.name}</p>
-              <div>{dailyWater ? (<p>Did you water me today?</p>) : ('')}</div>
-            </div>
+        <div css={tw`md:flex p-12`}>
+          <div css={tw`md:flex-shrink-0`}>
+            <img
+              css={tw`rounded-lg md:w-56`}
+              src="https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_snake-plant-slaurentii_terracotta_4_1024x1024.jpg?v=1537308389"
+              alt="Snake plant"
+            />
           </div>
-        </Link>
+          <div css={tw`mt-4 md:mt-0 md:ml-6 align-middle`}>
+            <Link to={`plant/${plant.id}`}>
+              <div
+                css={tw`uppercase tracking-wide text-sm text-indigo-600 font-bold`}
+              >
+                {plant.nickname}
+              </div>
+            </Link>
+            <div
+              href="#"
+              css={tw`block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline`}
+            >
+              {plant.water}
+            </div>
+            <p css={tw`mt-2 text-gray-600`}>{plant.name}</p>
+            <div>{dailyWater ? <p>Did you water me today?</p> : ""}</div>
+          </div>
+        </div>
+        <button
+          css={tw`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full`}
+        >
+          Yes
+        </button>
       </div>
     );
   });
@@ -38,9 +54,7 @@ const PlantList = () => {
   return (
     <div>
       <div css={tw`pt-6 pl-12 text-3xl`}>My garden</div>
-      <div>
-        {allPlants}
-      </div>
+      <div>{allPlants}</div>
     </div>
   );
 };
